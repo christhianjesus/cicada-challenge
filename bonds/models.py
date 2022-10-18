d70c1d11897f75587c5c925c5965def751900839
Christhian Guevara <christhianjesus@live.com>
2022-10-17 23:21:41 -0500

    cleaning folder structure

--- bonds/models.py
+++ bonds/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 
 class Bond(models.Model):
     owner = models.ForeignKey('auth.User', related_name='bounds_for_sale', on_delete=models.CASCADE)
@@ -11,8 +7,3 @@ class Bond(models.Model):
     price = models.DecimalField(max_digits=13, decimal_places=4)
     created = models.DateTimeField(auto_now_add=True)
     bought = models.DateTimeField(auto_now=True)
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
